0999063a8cc2d85fdcd25d146f1fedccdbec0eaf
JAlizadeh <jalizade3d@yahoo.com>
2018-05-04 18:38:39 +0200

    SDP Week1-07 finished

--- Courses/System & Device Programming/Examples/u07s04-Semaphores - page 64.c
+++ Courses/System & Device Programming/Examples/u07s04-Semaphores - page 64.c
@@ -4,6 +4,48 @@
 #include <semaphore.h>
 #include <pthread.h>
 
+void semaphoreWait (int *S) {
+			char ctr;
+
+			//If the pipe is empty, read() waits
+			if (read (S[0], &ctr, sizeof(char)) != 1) {
+				printf (“Error”);
+				exit (-1);
+			}
+
+			return;
+		}
+
+
+void semaphoreSignal (int *S) {
+			char ctr = 'X';
+
+			//Writes a single character, i.e., increments the semaphore counter k
+			if (write(S[1], &ctr, sizeof(char)) != 1) {
+				printf (“Error”);
+				exit (-1);
+			}
+			return;
+		}
+
+
+		void semaphoreInit (int *S, int k) {
+			char ctr = 'X';
+			int i;
+			if (pipe (S) == -1) {
+				printf (“Error”);
+				exit (-1);
+			}
+
+			// Writes k characters, i.e., initializes the semaphore counter to k
+			for(i=0;i<k,i++)
+				if (write(S[1], &ctr, sizeof(char)) != 1) {
+				printf (“Error”);
+				exit (-1);
+			}
+			return;
+		}		
+
 int main() {
 	int S[2];
 	pid_t pid;
